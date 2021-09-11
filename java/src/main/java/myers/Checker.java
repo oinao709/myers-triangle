abd8216eaa374ef33501f9e48b439df439159477
oinao709 <oinao777@gmail.com>
2021-09-11 13:27:43 +0900

    javaで新規作成

--- java/src/main/java/myers/Checker.java
+++ java/src/main/java/myers/Checker.java
@@ -5,7 +5,35 @@ import java.util.List;
 public class Checker {
     // 引数と戻り値は変更してはいけない
     public static String apply(List<String> ss) {
-        // 実装はクラス・メソッド・例外など何をどう使っても良い
-        return "正三角形";
+        int max = 0;
+        int min = 0;
+        if (ss.size() != 3) {
+            return "入力が不正です";
+        }
+
+        int[] ints = ss.stream().mapToInt(Integer::parseInt).sorted().toArray();
+
+//        // debug
+//        System.out.println(ints[0]);
+//        System.out.println(ints[1]);
+//        System.out.println(ints[2]);
+//
+        if (ints[0] == 0) {
+            return "入力が不正です";
+        }
+
+        if (ints[2] >= ints[0] + ints[1]) {
+            return "不成立";
+        }
+
+        if (ints[0] == ints[2]) {
+            return "正三角形";
+        } else {
+            if (ints[1] == ints[0] || ints[2] == ints[1]) {
+                return "二等辺三角形";
+            } else {
+                return "不等辺三角形";
+            }
+        }
     }
 }
