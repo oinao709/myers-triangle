38b1ffb7a6f188e567e319205835b7a2ce5ad6d8
oinao709 <oinao777@gmail.com>
2021-09-18 10:00:11 +0900

    例外処理追加

--- java/src/main/java/myers/Checker.java
+++ java/src/main/java/myers/Checker.java
@@ -11,29 +11,33 @@ public class Checker {
             return "入力が不正です";
         }
 
-        int[] ints = ss.stream().mapToInt(Integer::parseInt).sorted().toArray();
-
+       try {
+           int[] ints = ss.stream().mapToInt(Integer::parseInt).sorted().toArray();
 //        // debug
 //        System.out.println(ints[0]);
 //        System.out.println(ints[1]);
 //        System.out.println(ints[2]);
 //
-        if (ints[0] == 0) {
-            return "入力が不正です";
+           if (ints[0] == 0) {
+               return "入力が不正です";
+           }
+
+           if (ints[2] >= ints[0] + ints[1]) {
+               return "不成立";
+           }
+
+           if (ints[0] == ints[2]) {
+               return "正三角形";
+           } else {
+               if (ints[1] == ints[0] || ints[2] == ints[1]) {
+                   return "二等辺三角形";
+               } else {
+                   return "不等辺三角形";
+               }
+           }
+       } catch(Exception e) {
+           return "入力が不正です";
         }
 
-        if (ints[2] >= ints[0] + ints[1]) {
-            return "不成立";
-        }
-
-        if (ints[0] == ints[2]) {
-            return "正三角形";
-        } else {
-            if (ints[1] == ints[0] || ints[2] == ints[1]) {
-                return "二等辺三角形";
-            } else {
-                return "不等辺三角形";
-            }
-        }
     }
 }
